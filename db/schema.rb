877c056a7a001e0cff18ad4efc2e4396213995e6
Domingo Jose Ilarreta Heydras <domingo@greenti.cl>
2017-07-07 01:43:36 -0400

    Orden de Tablas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707023243) do
+ActiveRecord::Schema.define(version: 20170707105241) do
 
   create_table "abilities", force: :cascade do |t|
     t.string   "name"
